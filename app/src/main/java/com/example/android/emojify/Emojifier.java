f36a88eda9c188224c562acff44e3e8039cb5a68
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-13 01:35:35 -0500

    Finish TLIB.02

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.util.SparseArray;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
@@ -21,5 +22,11 @@ public class Emojifier {
         Frame frame = new Frame.Builder().setBitmap(bitmap).build();
         SparseArray<Face> faces = faceDetector.detect(frame);
         Log.i(Emojifier.class.getSimpleName(), "Number of faces = " + faces.size());
+
+        if(faces.size() == 0){
+            Toast.makeText(context, context.getResources().getString(R.string.no_faces_message), Toast.LENGTH_SHORT).show();
+        }
+
+        faceDetector.release();
     }
 }
