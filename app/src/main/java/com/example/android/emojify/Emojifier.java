dad459d207c11c168a6c720015a2986e4bf466c4
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-13 01:27:04 -0500

    Finish TLIB.02

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,25 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+/**
+ * Created by sudharti on 11/13/17.
+ */
+
+public class Emojifier {
+    public static void detectFaces(Context context, Bitmap bitmap) {
+        FaceDetector faceDetector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .build();
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Face> faces = faceDetector.detect(frame);
+        Log.i(Emojifier.class.getSimpleName(), "Number of faces = " + faces.size());
+    }
+}
